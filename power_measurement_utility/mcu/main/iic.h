51c2aceb66082f169f0342a34a6fa31340cbf25f
Asbjørn Djupdal <djupdal@ntnu.no>
2018-11-28 13:31:48 +0100

    Added missing files

---
+++ power_measurement_utility/mcu/main/iic.h
@@ -0,0 +1,40 @@
+/******************************************************************************
+ *
+ *  This file is part of the Lynsyn PMU firmware.
+ *
+ *  Copyright 2018 Asbjørn Djupdal, NTNU, TULIPP EU Project
+ *
+ *  Lynsyn is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Lynsyn is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Lynsyn.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *****************************************************************************/
+
+#ifndef IIC_H
+#define IIC_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <em_i2c.h>
+
+#define I2C_NO_CMD               0
+#define I2C_MAGIC                1
+#define I2C_READ_CURRENT_AVG     2
+#define I2C_READ_CURRENT_INSTANT 3
+#define I2C_GET_CAL              4
+
+extern uint64_t i2cCurrentAcc[7];
+extern int i2cSamplesSinceLast[7];
+extern int16_t i2cCurrentInstant[7];
+
+#endif
