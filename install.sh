493a3661a9eeed0e4da7e4bbbd2ed2c173b1584c
Asbjørn Djupdal <djupdal@ntnu.no>
2018-12-17 14:06:22 +0100

    Added support for 18.10

--- install.sh
+++ install.sh
@@ -10,6 +10,9 @@ if [[ $RELEASE == "17.10" ]]; then
 elif [[ $RELEASE == "18.04" ]]; then
     sudo apt install build-essential qt5-default libqt5charts5-dev libqt5sql5 libqt5sql5-sqlite libusb-1.0-0-dev llvm-6.0 llvm-6.0-dev clang-6.0 libclang-6.0-dev clang llvm
 
+elif [[ $RELEASE == "18.10" ]]; then
+    sudo apt install build-essential qt5-default libqt5charts5-dev libqt5sql5 libqt5sql5-sqlite libusb-1.0-0-dev llvm-6.0 llvm-6.0-dev clang-6.0 libclang-6.0-dev clang llvm
+
 else
     echo "Ubuntu version not supported.  Please do a manual install."
     exit
