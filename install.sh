a05c501f0293c7fe925b6fcc5c7fc24e2e38e893
Asbjørn Djupdal <djupdal@ntnu.no>
2018-12-17 10:18:21 +0100

    Added install script

---
+++ install.sh
@@ -0,0 +1,28 @@
+# Install script for TULIPP STHEM
+
+#!/bin/bash
+
+RELEASE=`lsb_release -a 2>/dev/null | grep Release | awk '{ print $2 }'`
+
+if [[ $RELEASE == "17.10" ]]; then
+    sudo apt install build-essential qt5-default libqt5charts5-dev libqt5sql5 libqt5sql5-sqlite libusb-1.0-0-dev llvm-5.0 llvm-5.0-dev clang-5.0 libclang-5.0-dev clang llvm
+
+elif [[ $RELEASE == "18.04" ]]; then
+    sudo apt install build-essential qt5-default libqt5charts5-dev libqt5sql5 libqt5sql5-sqlite libusb-1.0-0-dev llvm-6.0 llvm-6.0-dev clang-6.0 libclang-6.0-dev clang llvm
+
+else
+    echo "Ubuntu version not supported"
+fi
+
+cd analysis_utility
+make
+cd ..
+
+echo "#BEGIN TulippProfile" >> ~/.profile
+echo export PATH=\$PATH:`pwd`/bin >> ~/.profile
+echo "#END TulippProfile" >> ~/.profile
+
+cd power_measurement_utility
+sudo make install_hw
+cd ..
+
