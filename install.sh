4e35091316c1507281038210b358093a0248cc1e
Asbjørn Djupdal <djupdal@ntnu.no>
2018-12-18 09:38:25 +0100

    Updated install script

--- install.sh
+++ install.sh
@@ -5,18 +5,20 @@
 RELEASE=`lsb_release -a 2>/dev/null | grep Release | awk '{ print $2 }'`
 
 if [[ $RELEASE == "17.10" ]]; then
-    sudo apt install build-essential qt5-default libqt5charts5-dev libqt5sql5 libqt5sql5-sqlite libusb-1.0-0-dev llvm-5.0 llvm-5.0-dev clang-5.0 libclang-5.0-dev clang llvm
+    echo "Ubuntu 17.10"
 
 elif [[ $RELEASE == "18.04" ]]; then
-    sudo apt install build-essential qt5-default libqt5charts5-dev libqt5sql5 libqt5sql5-sqlite libusb-1.0-0-dev llvm-6.0 llvm-6.0-dev clang-6.0 libclang-6.0-dev clang llvm
+    echo "Ubuntu 18.04"
 
 elif [[ $RELEASE == "18.10" ]]; then
-    sudo apt install build-essential qt5-default libqt5charts5-dev libqt5sql5 libqt5sql5-sqlite libusb-1.0-0-dev llvm-6.0 llvm-6.0-dev clang-6.0 libclang-6.0-dev clang llvm
+    echo "Ubuntu 18.10"
 
 else
     echo "Ubuntu version not supported.  Please do a manual install."
     exit
 fi
+
+sudo apt install build-essential qt5-default libqt5charts5-dev libqt5sql5 libqt5sql5-sqlite libusb-1.0-0-dev llvm llvm-dev clang libclang-dev
 
 cd analysis_utility
 make
