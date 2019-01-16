9e4342361381046245dd6c553cde77d2d669dc6f
Björn Gottschall <git.mail@bgottschall.de>
2019-01-16 14:31:00 +0100

    toolsettings for wrapper

--- analysis_utility/wrapper/toolsettings.sh
+++ analysis_utility/wrapper/toolsettings.sh
@@ -1,7 +1,7 @@
-WRAPPER="wrapper"
-CLANG="clang"
-CLANGPP="clang++"
-LLVM_IR_PARSER="llvm_ir_parser"
-OPT="opt"
-LLC="llc"
-AS="aarch64-hipperos-as"
+WRAPPER=${WRAPPER:-"wrapper"}
+CLANG=${CLANG:-"clang"}
+CLANGPP=${CLANGPP:-"clang++"}
+LLVM_IR_PARSER=${LLVM_IR_PARSER:-"llvm_ir_parser"}
+OPT=${OPT:-"opt"}
+LLC=${LLC:-"llc"}
+AS=${AS:-"aarch64-hipperos-as"}
