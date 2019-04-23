fc8d00c4df599ca508011382b0d094297fca126b
Björn Gottschall <git.mail@bgottschall.de>
2019-04-23 14:44:27 +0200

    support for full vmmap

--- target/pmapelf/pmapelf.c
+++ target/pmapelf/pmapelf.c
@@ -10,114 +10,48 @@
 #include <sys/user.h>
 #include <spawn.h>
 #include <getopt.h>
+#include <stdbool.h>
 
-static const void *PTRACE_NO_SIGNAL = 0;
-static const void *PTRACE_IGNORE_PTR;
+#include "vmmap.h"
 
-extern char **environ;
-
-struct VMMaps {
-  unsigned int count;
-  uint64_t* start;
-  uint64_t* length;
+struct pidList {
+    uint64_t count;
+    pid_t *pids;
 };
 
-pid_t* getPIDs(unsigned int *count) {
-  pid_t* result = NULL;
-  int pid;
-  *count = 0;
-  unsigned int pidAlloc = 1;
-  FILE *ps = popen("ps -e | tail -n+2 | awk '{print $1}'", "r");
-  while (fscanf(ps,"%d", &pid) == 1) {
-    if (*count == 0) {
-      result = (pid_t *) malloc(pidAlloc * sizeof(pid_t));
-    } else if (pidAlloc <= *count) {
-      pidAlloc *= 2;
-      result = (pid_t *) realloc(result, pidAlloc * sizeof(pid_t));
+struct pidList getPIDList() {
+    struct pidList result = {};
+    unsigned int pidAlloc = 1;
+    int pid;
+    FILE *ps = popen("ps -e | tail -n+2 | awk '{print $1}'", "r");
+    while (fscanf(ps,"%d", &pid) == 1) {
+        if (result.count == 0) {
+            result.pids = (pid_t *) malloc(pidAlloc * sizeof(pid_t));
+        } else if (pidAlloc <= result.count) {
+            pidAlloc *= 2;
+            result.pids = (pid_t *) realloc(result.pids, pidAlloc * sizeof(pid_t));
+        }
+        if (result.pids == NULL) break;
+        result.pids[result.count] = pid;
+        result.count++;
     }
-    if (result == NULL) break;
-    result[(*count)] = pid;
-    (*count) = (*count) + 1;
-  }
-  pclose(ps);
-
-  if (result == NULL) {
+    pclose(ps);
+    
+    if (result.pids == NULL) {
+        result.count = 0;
+        return result;
+    }
+    
+    result.pids = (pid_t *) realloc(result.pids, result.count * sizeof(pid_t));
     return result;
-  }
-  
-  result = (pid_t *) realloc(result, (*count) * sizeof(pid_t));
-  return result;
 }
 
-struct VMMaps getProcessVMMaps(pid_t pid, unsigned int const limit) {
-  unsigned int allocCount = 1;
-  struct VMMaps result;
-  result.count = 0;
-  result.start = NULL;
-  result.length = NULL;
-  uint64_t start;
-  uint64_t length;
-  char cmd[1024];
-  if (snprintf(cmd,1024,"pmap -q -d %d | awk '$3 ~ /x/ {print \"0x\"$1\" \"$2}'", pid) <= 0) {
-    return result;
-  }
-  FILE *ps = popen(cmd, "r");
-  while (fscanf(ps,"%lx %lu", &start, &length) == 2) {
-    if (result.count == 0) {
-      result.start = (uint64_t *) malloc(allocCount * sizeof(uint64_t));
-      result.length = (uint64_t *) malloc(allocCount * sizeof(uint64_t));
-    } else if (allocCount <= result.count) {
-      allocCount *= 2;
-      result.start = (uint64_t *) realloc(result.start, allocCount * sizeof(uint64_t));
-      result.length = (uint64_t *) realloc(result.length, allocCount * sizeof(uint64_t));
+void freePIDList(struct pidList list) {
+    if (list.pids != NULL && list.count > 0) {
+        free(list.pids);
     }
-    if (result.start == NULL || result.length == NULL) {
-      break;
-    }
-    result.start[result.count] = start;
-    result.length[result.count] = length * 1024;
-    result.count = result.count + 1;
-    if (limit == result.count) break;
-  }
-  pclose(ps);
-  
-  if (result.start == NULL || result.length == NULL) {
-    if (result.start != NULL) free(result.start);
-    if (result.length != NULL) free(result.length);
-    result.count = 0;
-    return result;
-  }
-  
-  result.start = (uint64_t *) realloc(result.start, result.count * sizeof(uint64_t));
-  result.length = (uint64_t *) realloc(result.length, result.count * sizeof(uint64_t));
-  
-  return result;
 }
 
-void freeVMMaps( struct VMMaps map ) {
-  if (map.start != NULL) free(map.start);
-  if (map.length != NULL) free(map.length);
-}
-
-void dumpVMMaps( struct VMMaps map ) {
-  for (unsigned int i = 0; i < map.count; i++) {
-    printf("%02d: 0x%lx - 0x%lx\n", i, map.start[i], map.start[i] + map.length[i]);
-  }
-}
-
-int VMMapCollision(struct VMMaps map1, struct VMMaps map2) {
-  for (unsigned int i = 0; i < map1.count; i++) {
-    uint64_t const m1start = map1.start[i];
-    uint64_t const m1end = m1start + map1.length[i];
-    for (unsigned int j = 0; j < map2.count; j++) {
-      uint64_t const m2start = map2.start[j];
-      uint64_t const m2end = map2.start[j] + map2.length[j];
-      if (m1start >= m2start && m1start < m2end) return 1;
-      if (m1end >= m2start && m1end < m2end) return 1;
-    }
-  }
-  return 0;
-}
 
 void help(char const opt, char const *optarg) {
     FILE *out = stdout; 
@@ -132,29 +66,34 @@ void help(char const opt, char const *optarg) {
     fprintf(out, "pmapelf [options] -- <command> [arguments]\n");
     fprintf(out, "\n");
     fprintf(out, "Options:\n");
-    fprintf(out, "  -o, --output=<file>       write pmap to file\n");
-    fprintf(out, "  -c, --collision           check on colissions\n");
+    fprintf(out, "  -v, --vmmap=<file>        write vm map to file\n");
+    fprintf(out, "  -o, --offset=<file>       write process offset to file\n");
+    fprintf(out, "  -d, --deattach            deattach immediatly, skips vmmap!\n");
     fprintf(out, "  -k, --key                 continue with keypress\n");
     fprintf(out, "  -t, --time=<milliseconds> continue after time\n");
-    fprintf(out, "  -s, --short               output mapped text region\n");
+    fprintf(out, "  -c, --collision           check on colissions\n");
     fprintf(out, "\n");
     fprintf(out, "Example: pmapelf -o /tmp/map -c -k -- stress-ng --cpu 4\n");
 }
 
 int main(int const argc, char **argv) {
-    char *outputFilename = NULL;
-    unsigned long waitTime = 0;
-    int keyPress = -1;
+    FILE *offsetout = stdout; 
+    FILE *vmmapout = stdout; 
+    char *vmmapFilename = NULL;
+    char *offsetFilename = NULL;
     char **argsStart = NULL;
-    int collisionDetection = -1;
-    int shortOutput = -1;
+    bool keyPress = false;
+    unsigned long waitTime = 0;
+    bool collisionDetection = false;
+    bool deattach = false;
     
     static struct option const long_options[] =  {
         {"help",      no_argument, 0, 'h'},
-        {"short",     no_argument, 0, 's'},
         {"key",       no_argument, 0, 'k'},
         {"collision", no_argument, 0, 'c'},
-        {"output",    required_argument, 0, 'o'},
+        {"deattach",  no_argument, 0, 'd'},
+        {"offset",    required_argument, 0, 'o'},
+        {"vmmap",     required_argument, 0, 'v'},
         {"time",      required_argument, 0, 't'},
         {0, 0, 0, 0}
     };
@@ -162,10 +101,11 @@ int main(int const argc, char **argv) {
     while (1) {
         char *endptr;
         int c;
+        
         int option_index = 0;
         size_t len = 0;
         
-        c = getopt_long (argc, argv, "skcho:t:", long_options, &option_index);
+        c = getopt_long (argc, argv, "dkchv:o:t:", long_options, &option_index);
         if (c == -1) {
             break;
         }
@@ -183,28 +123,42 @@ int main(int const argc, char **argv) {
                     return 1;
                 }
                 break;
+            case 'v':
+                len = strlen(optarg);
+                if (strlen(optarg) == 0) {
+                    help(c ,optarg);
+                    return 1;
+                }
+                vmmapFilename = (char *) malloc(len + 1);
+                if (vmmapFilename == NULL) {
+                    fprintf(stderr, "Memory allocation of %ld bytes failed!\n", len);
+                    return 1;
+                }
+                memset(vmmapFilename, 0, len + 1);
+                strncpy(vmmapFilename, optarg, len);
+                break;
             case 'o':
                 len = strlen(optarg);
                 if (strlen(optarg) == 0) {
                     help(c ,optarg);
                     return 1;
                 }
-                outputFilename = (char *) malloc(len + 1);
-                if (outputFilename == NULL) {
+                offsetFilename = (char *) malloc(len + 1);
+                if (offsetFilename == NULL) {
                     fprintf(stderr, "Memory allocation of %ld bytes failed!\n", len);
                     return 1;
                 }
-                memset(outputFilename, 0, len + 1);
-                strncpy(outputFilename, optarg, len);
+                memset(offsetFilename, 0, len + 1);
+                strncpy(offsetFilename, optarg, len);
                 break;
             case 'k':
-                keyPress = 1;
+                keyPress = true;
                 break;
-            case 's':
-                shortOutput = 1;
+            case 'd':
+                deattach = true;
                 break;
             case 'c':
-                collisionDetection = 1;
+                collisionDetection = true;
                 break;
             default:
                 abort();
@@ -224,6 +178,12 @@ int main(int const argc, char **argv) {
     }
 
     pid_t targetPid;
+    pid_t intrTarget;
+
+    int ret = 0;
+
+    long rptrace;
+    uint64_t interrupts = 0;
     
     do {
         targetPid = fork();
@@ -231,118 +191,165 @@ int main(int const argc, char **argv) {
 
     if (targetPid == -1) {
         fprintf(stderr, "ERROR: could not fork!\n");
-        return 1;
+        ret = 1; goto exit; 
     }
 
     if (targetPid == 0) {
-        ptrace(PTRACE_TRACEME, PTRACE_IGNORE_PTR, PTRACE_IGNORE_PTR, PTRACE_IGNORE_PTR);
-        execvp(argsStart[0], argsStart);
+        if (ptrace(PTRACE_TRACEME, NULL, NULL, NULL) == -1) {
+            fprintf(stderr, "ERROR: ptrace traceme failed!\n");
+            return 1;
+        }
+        if (execvp(argsStart[0], argsStart) != 0) {
+            fprintf(stderr, "ERROR: failed to execute");
+            for (unsigned int i = 0; argsStart[i] != NULL; i++) {
+                fprintf(stderr, " %s", argsStart[i]);
+            }
+            fprintf(stderr, "\n");
+            return 1;
+            
+        }
     } else {
         int status;
-        waitpid(targetPid, &status, 0);
+
+        do {
+            intrTarget = waitpid(targetPid, &status, 0);
+        } while (intrTarget == -1 && errno == EAGAIN);
+
         if (WIFEXITED(status)) {
             fprintf(stderr,"ERROR: unexpected process termination\n");
-            return 2;
+            ret = 2; goto exit;
         }
 
-        if (collisionDetection > 0 || shortOutput > 0) {
-            struct VMMaps targetMap = getProcessVMMaps(targetPid, 1);
-            if (targetMap.count == 0) {
-                fprintf(stderr, "ERROR: could not find virtual memory mapping!\n");
-                kill(targetPid, SIGKILL);
-                return 1;
-            }
 
-            if (collisionDetection > 0) {
-                unsigned int pidCount = 0;
-                pid_t *pids = getPIDs(&pidCount);
+        if (ptrace(PTRACE_SETOPTIONS, targetPid, NULL,  PTRACE_O_EXITKILL | PTRACE_O_TRACEEXIT) == -1) {
+            fprintf(stderr, "ERROR: Could not set ptrace options!\n");
+            ret = 1; goto exitWithTarget;
+        }
+        
+        struct VMMaps targetMap = getProcessVMMaps(targetPid, 1);
+        if (targetMap.count == 0) {
+            fprintf(stderr, "ERROR: could not find process memory mapping!\n");
+            ret = 1; goto exitWithTarget;
+        }
 
-                for (unsigned int i = 0; i < pidCount; i++) {
-                    if (targetPid == pids[i]) continue;
-                    struct VMMaps checkMap = getProcessVMMaps(pids[i],0);
-                    if (VMMapCollision(targetMap, checkMap)) {
-                        fprintf(stderr, "ERROR: current virtual memory mapping collides with pid %d\n", pids[i]);
-                        freeVMMaps(checkMap);
-                        freeVMMaps(targetMap);
-                        free(pids);
-                        kill(targetPid, SIGKILL);
-                        return 1;
-                    }
-                    freeVMMaps(checkMap);
-                }
-                free(pids);
-            }
-
-            if (shortOutput > 0) {
-                FILE *out = stdout;
-
-                if (outputFilename != NULL) {
-                    out = fopen(outputFilename, "w+");
-                    if (out == NULL) {
-                        fprintf(stderr, "ERROR: Could not open %s for writing!\n", outputFilename);
-                        free(outputFilename);
-                        return 1;
-                    }
-                }
-
-                for (unsigned int i = 0; i < targetMap.count; i++) {
-                    fprintf(out, "0x%lx - 0x%lx\n", targetMap.start[i], targetMap.start[i] + targetMap.length[i]);
-                }
-                
-                if (outputFilename != NULL) {
-                    fclose(out);
-                    free(outputFilename);
+        { //Output Offset
+            if (offsetFilename != NULL) {
+                offsetout = fopen(offsetFilename, "w+");
+                if (offsetout == NULL) {
+                    fprintf(stderr, "ERROR: Could not open %s for writing!\n", offsetFilename);
+                    ret = 1; goto exitWithTarget;
                 }
             }
             
-            freeVMMaps(targetMap);
+            fprintf(offsetout, "0x%016lx 0x%016lx %s\n", targetMap.maps[0].addr, targetMap.maps[0].size, targetMap.maps[0].label);
+            
+            if (offsetFilename != NULL) {
+                fclose(offsetout);
+            }
         }
-
-
-        if (shortOutput <= 0) {
-            char tplStd[] = "pmap -d -q %d";
-            char tplFile[] = "pmap -d -q %d 2>&1 >%s";
-            char *tpl = tplStd;
-            char *cmd;
-            int alloc;
-            if (outputFilename != NULL) {
-                tpl = tplFile;
-            }
-            alloc = snprintf(cmd, 0, tpl, targetPid, outputFilename);
-            cmd = (char *) malloc((++alloc) * sizeof(char *));
-            if (cmd == NULL) {
-                fprintf(stderr, "ERROR: could not allocate %d bytes", alloc);
-                kill(targetPid, SIGKILL);
-                return 1;
-            }
-            snprintf(cmd, alloc, tpl, targetPid, outputFilename);
-            if (system(cmd) != 0) {
-                fprintf(stderr, "ERROR: could not execute %s", cmd);
-                kill(targetPid, SIGKILL);
-                return 1;
-            }
-            free(cmd);
-        }
-
         
+        if (collisionDetection) {
+            struct pidList list = getPIDList();
+            for (unsigned int i = 0; i < list.count; i++) {
+                if (targetPid == list.pids[i]) continue;
+                struct VMMaps checkMap = getProcessVMMaps(list.pids[i], 0);
+                if (VMMapCollision(targetMap, checkMap)) {
+                    fprintf(stderr, "ERROR: current virtual memory mapping collides with pid %d\n", list.pids[i]);
+                    freeVMMaps(checkMap);
+                    freeVMMaps(targetMap);
+                    freePIDList(list);
+                    ret = 1; goto exitWithTarget;
+                }
+                freeVMMaps(checkMap);
+            }
+            freePIDList(list);
+        }
+        
+        freeVMMaps(targetMap);
+
         if (keyPress <= 0 && waitTime > 0) {
             usleep(waitTime * 1000);
         } else if (keyPress > 0) {
             fgetc(stdin);
         }
 
-        ptrace(PTRACE_DETACH, targetPid, PTRACE_IGNORE_PTR, PTRACE_NO_SIGNAL);
-        waitpid(targetPid, &status, 0);
-        if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
-            return 0;
-        } else {
-            return 2;
+        if (!deattach) {
+            do {
+                rptrace = ptrace(PTRACE_CONT, targetPid, NULL, NULL);
+                if (rptrace == -1 && errno == ESRCH) {
+                    fprintf(stderr, "ERROR: death under ptrace!\n");
+                    ret = 1; goto exit;
+                }
+                
+                do {
+                    intrTarget = waitpid(targetPid, &status, 0);
+                } while (intrTarget == -1 && errno == EAGAIN);
+                
+                interrupts++;
+                //fprintf(stderr, "DEBUG: interrupt signal %d\n", WSTOPSIG(status));
+                
+            } while (status>>8 != (SIGTRAP | (PTRACE_EVENT_EXIT<<8)));
+            
+            
+            targetMap = getProcessVMMaps(targetPid, 0);
+            if (targetMap.count == 0) {
+                fprintf(stderr, "ERROR: could not find final process memory mapping!\n");
+                ret = 1; goto exitWithTarget;
+            }
+            
+            { // VMMap Output
+                if (vmmapFilename != NULL) {
+                    vmmapout = fopen(vmmapFilename, "w+");
+                    if (vmmapout == NULL) {
+                        fprintf(stderr, "ERROR: Could not open %s for writing!\n", offsetFilename);
+                        ret = 1; goto exitWithTarget;
+                    }
+                }
+                
+                for( unsigned int i = 0; i < targetMap.count; i++) { 
+                    fprintf(vmmapout, "0x%016lx 0x%016lx %s\n", targetMap.maps[i].addr, targetMap.maps[i].size, targetMap.maps[i].label);
+                }
+                
+                if (vmmapFilename != NULL) {
+                    fclose(vmmapout);
+                }
+            }
+            
+            freeVMMaps(targetMap);
         }
+
+        ptrace(PTRACE_DETACH, targetPid, NULL, NULL, NULL);
+        do {
+            intrTarget = waitpid(targetPid, &status, 0);
+        } while (intrTarget == -1 && errno == EAGAIN);
+
+        if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
+            ret = 0;
+        } else {
+            ret = 2;
+        }
+
+        
     }
 
-    if (outputFilename) {
-        free(outputFilename);
+    if (interrupts > 1) {
+        fprintf(stderr, "WARNING: %ld unexpected appplication interrupts occured, non-intrusive execution was not assured!\n", interrupts);
     }
 
-    return 0;
+    goto exit;
+
+ exitWithTarget:
+    kill(targetPid, SIGKILL);
+    ptrace(PTRACE_DETACH, targetPid, NULL, NULL);
+ exit:
+    
+    if (vmmapFilename) {
+        free(vmmapFilename);
+    }
+
+    if (offsetFilename) {
+        free(offsetFilename);
+    }
+
+    return ret;
 }
